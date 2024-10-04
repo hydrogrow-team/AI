91a067d243bc69032f001984f445ef26eb4f85de
Marwan Walid <marwan-walid1@hotmail.com>
2024-10-04 15:15:45 +0300

    Saved models

---
+++ predictions.py
@@ -0,0 +1,10 @@
+import pickle
+
+with open("DecisionTree.pkl",'rb') as f:
+    DecisionTree = pickle.load(f)
+with open("RandomForest.pkl",'rb') as f:
+    Rf = pickle.load(f)
+
+print(DecisionTree.predict([[113,38,25,22,79,7.388,90.422]]))
+
+print(Rf.predict([[113,38,25,22,79,7.388,90.422]]))
