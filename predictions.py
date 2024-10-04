e3d8b39c3f697d25159b3043850f32a601ac1720
Marwan Walid <marwan-walid1@hotmail.com>
2024-10-04 17:53:25 +0300

    added flask

--- predictions.py
+++ predictions.py
@@ -5,6 +5,3 @@ with open("DecisionTree.pkl",'rb') as f:
 with open("RandomForest.pkl",'rb') as f:
     Rf = pickle.load(f)
 
-print(DecisionTree.predict([[113,22,79,7.388,90.422]]))
-
-print(Rf.predict([[113,22,79,7.388,90.422]]))
