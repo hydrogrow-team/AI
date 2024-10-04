4a804dd83b4047fa31af2b34521d5e4bb346bc65
Marwan Walid <marwan-walid1@hotmail.com>
2024-10-04 18:32:15 +0300

    added flask

---
+++ app.py
@@ -0,0 +1,15 @@
+import jsonify.convert
+import flask,requests,pickle,jsonify
+
+app = flask.Flask(__name__)
+
+@app.route('/',methods=['GET'])
+def send_data():
+    data = requests.get("")
+    with open("RandomFores.pkl",'rb') as f:
+        model = pickle.load(f)
+    predictions = model.predict_proba(data)
+    return predictions
+
+
+    pass
